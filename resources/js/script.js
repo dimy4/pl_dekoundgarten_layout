c532e9406377efd92bf164c77495e4973c9aef8b
plentylionsgit <info@plentylions.de>
2019-08-02 16:57:36 +0300

    dev

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,22 +1,22 @@
-Vue.filter("variationSelectIsDisabled", function(selectedAttributes, attributeId, attributeValueId, variations)
-{
-    const attributes = JSON.parse(JSON.stringify(selectedAttributes));
-    attributes[attributeId] = attributeValueId;
+Vue.filter("variationSelectIsDisabled", function (selectedAttributes, attributeId, attributeValueId, variations) {
+  const attributes = JSON.parse(JSON.stringify(selectedAttributes));
+  attributes[attributeId] = attributeValueId;
 
-    let result = variations.filter(variation =>
-    {
-        for (let i = 0; i < variation.attributes.length; i++)
-        {
-            const id = variation.attributes[i].attributeId;
-            const val = variation.attributes[i].attributeValueId;
+  let result = variations.filter(variation => {
+    for (let i = 0; i < variation.attributes.length; i++) {
+      const id = variation.attributes[i].attributeId;
+      const val = variation.attributes[i].attributeValueId;
 
-            if (!!attributes[id] && attributes[id] != val)
-            {
-                return false;
-            }
-        }
+      if (!!attributes[id] && attributes[id] != val) {
+        return false;
+      }
 
-        return variation.attributes.length > 0;
-    });
-    return result.length > 0
+      if (attributes[id] && attributes[id] == val && !variation.isSalable) {
+        return false;
+      }
+    }
+
+    return variation.attributes.length > 0;
+  });
+  return result.length <= 0;
 });
