6067aad0c24c9a54ad0ebd2f8032e2c22f362293
plentylionsgit <info@plentylions.de>
2020-03-12 16:50:57 +0200

    dev

--- resources/js/script.js
+++ resources/js/script.js
@@ -20,3 +20,22 @@ Vue.filter("variationSelectIsDisabled", function (selectedAttributes, attributeI
   });
   return result.length <= 0;
 });
+
+function dekoAddToBasket(variationId, qty) {
+  const basketObject = {
+    variationId: variationId,
+    quantity: qty
+  };
+
+  window.ceresStore.dispatch("addBasketItem", basketObject).then(
+    response => {
+      document.dispatchEvent(new CustomEvent("afterBasketItemAdded", {
+        detail: basketObject
+      }));
+    },
+    error => {
+      if (error.data) {
+        window.CeresNotification.error("Failed add to basket.").closeAfter(2000);
+      }
+    });
+}
