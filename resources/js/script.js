eb99641b9f6ec3d2f93d01feb5e56696782b559d
plentylionsgit <info@plentylions.de>
2020-11-26 10:37:02 +0200

    5.0.15

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,26 +1,3 @@
-Vue.filter("variationSelectIsDisabled", function (selectedAttributes, attributeId, attributeValueId, variations) {
-  const attributes = JSON.parse(JSON.stringify(selectedAttributes));
-  attributes[attributeId] = attributeValueId;
-
-  let result = variations.filter(variation => {
-    for (let i = 0; i < variation.attributes.length; i++) {
-      const id = variation.attributes[i].attributeId;
-      const val = variation.attributes[i].attributeValueId;
-
-      if (!!attributes[id] && attributes[id] != val) {
-        return false;
-      }
-
-      if (attributes[id] && attributes[id] == val && !variation.isSalable) {
-        return false;
-      }
-    }
-
-    return variation.attributes.length > 0;
-  });
-  return result.length <= 0;
-});
-
 function dekoAddToBasket(variationId, qty) {
   const basketObject = {
     variationId: variationId,
